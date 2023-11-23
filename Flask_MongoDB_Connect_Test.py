56d6406223c351cf6d214e5f918f5e2cde361b13
Naveen chand Gadde <139424272+ChanduG97@users.noreply.github.com>
2023-11-23 18:02:40 +0530

    Add files via upload
    Flask framework and python code to connect to MongoDB and also used to push the data to the data base.
    Here, the update is that the End user can enter the number of employees as per user required and push that data to the Database

--- Flask_MongoDB_Connect_Test.py
+++ Flask_MongoDB_Connect_Test.py
@@ -1,23 +1,48 @@
 import pymongo
+from flask import Flask, jsonify
 client = pymongo.MongoClient('mongodb+srv://naveenchandgadde:ChanduG97@flask.0zcjxbr.mongodb.net/')
 mydb = client['Employee']
 information=mydb.table1
-records = [{
 
-        "Emp NO" : 501,
-        "Emp Name":"Chandu",
-        "Job":"Associate Software Engineer",
-        "Hire Date":"03-07-2023",
-        "Dept NO":2,
-        "Salary" : 15000
+n=int(input("Enter number of Employees : "))
+for i in range(n):
+    Emp_no=int(input("Enter Employee No : "))
+    Emp_Name=input("Enter Employee Name : ")
+    Job_Role=input("Enter Role of Employee : ")
+    Hire_Date=input("Enter Joining Date : ")
+    dept_no=int(input("Enter Dept No : "))
+    Sal=float(input("Enter your Salary : "))
+    records=[{
+        "Emp NO" : Emp_no,
+        "Emp Name" : Emp_Name,
+        "Job":Job_Role,
+        "Hire Date":Hire_Date,
+        "Dept NO":dept_no,
+        "Salary" : Sal
     }]
+    information.insert_many(records)
+    if i==n:
+        break
+
 information.insert_many(records)
 
-class Object1:
+class Employees:
     def __init__(self, name):
         self.name = name
         self.relationships = []
 
-class Object2:
+class Organization:
     def __init__(self, name):
         self.name = name
+
+app = Flask(__name__)
+@app.route('/api', methods=['GET'])
+def api():
+    data = {
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'New York'
+    }
+    return jsonify(data)
+if __name__ == '__main__':
+    app.run(debug=True)
